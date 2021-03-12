85dfbec02e34ec78532ee4f0b2a9e38b7256c898
Ycaro de Souza Lehmkuhl <65562799+ycarolehmkuhl@users.noreply.github.com>
2021-03-12 11:52:40 -0300

    new

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,54 +1,75 @@
-;(function () {
-	const todos = [
-		
-	]
+.js
+(function () {
+	const todos = [];
+
 	const app = new Vue({
-		el: '#app',
+		el: "#app",
 		data: {
-			todos:todos,
-			inputText: '',
+			todos: todos.filter(() => {
+				console.log(this.filter);
+				return true;
+			}),
+			filter: "all",
+			inputText: "",
 			currentEdit: null,
-			byebye: ''
+			byebye: "",
+		},
+		computed: {
+			filteredTodos: function () {
+				return this.todos.filter((todo) => {
+					if (this.filter === "all") {
+						return true;
+					}
+
+					if (this.filter === "todo") {
+						return !todo.done;
+					}
+
+					return todo.done;
+				});
+			},
 		},
 		methods: {
-			addTodos (e){
-				const {inputText,todos} = this
+			addTodos(e) {
+				const { inputText, todos } = this;
 				if (inputText.trim().length === 0) {
-					return
+					return;
 				}
-				const lastItem = todos[todos.length-1]
-				const id = lastItem ? lastItem.id + 1 : 1
+				const lastItem = todos[todos.length - 1];
+				const id = lastItem ? lastItem.id + 1 : 1;
 				this.todos.push({
-					id : id,
+					id: id,
 					title: inputText,
-					done: false
-				})
-				this.inputText = ''
-				
+					done: false,
+				});
+				this.inputText = "";
 			},
 
-			removeItem (index){
-				this.todos.splice(index,1)
+			removeItem(index) {
+				this.todos.splice(index, 1);
 			},
 
-			getEditing (item){
-				this.currentEdit = item
-				this.byebye = item.title
+			getEditing(item) {
+				this.currentEdit = item;
+				this.byebye = item.title;
 			},
 
-			saveEdit (item,index){
-				if(item.title.trim().length===0){
-					this.todos.splice(index,1)
-				}else{
-					this.currentEdit = ''
+			saveEdit(item, index) {
+				if (item.title.trim().length === 0) {
+					this.todos.splice(index, 1);
+				} else {
+					this.currentEdit = "";
 				}
 			},
 
-			cancelEdit (){
-				this.currentEdit.title = this.byebye
-				this.currentEdit = null
-			}
-		}
-	})
+			cancelEdit() {
+				this.currentEdit.title = this.byebye;
+				this.currentEdit = null;
+			},
 
+			setFilter(filter) {
+				this.filter = filter;
+			},
+		},
+	});
 })();
